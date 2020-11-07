48d49040c5083e9f98a2bb163e53eda9a4a222b0
Artyom Shikovetsky <tera1134@gmail.com>
2020-11-07 12:16:30 +0300

    init: finished main logic of game

---
+++ gem-puzzle/webpack.config.js
@@ -0,0 +1,100 @@
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const TerserWebpackPlugin = require('terser-webpack-plugin')
+const OptimizeAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin')
+
+const isDev = process.env.NODE_ENV === 'development'
+const isProd = !isDev
+
+const styleLoaders = (extendedLoader) => {
+  const loaders = [
+    {
+      loader: MiniCssExtractPlugin.loader,
+    },
+    'css-loader'
+  ]
+
+  if (extendedLoader)
+    loaders.push(extendedLoader)
+
+  return loaders
+}
+
+const optimizations = () => {
+  const config = {
+    splitChunks: {
+      chunks: 'all',
+    },
+    chunkIds: false
+  }
+
+  if (isProd)
+    config.minimizer = [
+      new TerserWebpackPlugin(),
+      new OptimizeAssetsWebpackPlugin()
+    ]
+
+  return config
+}
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  entry: ['@babel/polyfill', './app.js'],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: (chunk) => `app${isProd ? '.[contenthash]' : ''}.js`,
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    overlay: true,
+    compress: true,
+    stats: 'errors-only',
+  },
+  mode: process.env.NODE_ENV,
+  devtool: isDev ? 'source-map' : false,
+  optimization: optimizations(),
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: {
+          loader: 'html-loader',
+        }
+      },
+      {
+        test: /\.css$/,
+        use: styleLoaders()
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: styleLoaders('sass-loader')
+      },
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          }
+        }
+      }
+    ]
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    }
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HTMLWebpackPlugin({
+      template: 'index.html',
+      minify: isProd,
+    }),
+    new MiniCssExtractPlugin({
+      filename: `style${isProd ? '.[contenthash]' : ''}.css`,
+    }),
+  ]
+}
